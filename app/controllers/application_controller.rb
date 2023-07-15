b6585ff3de4973ef1e5059f604227962d3935a8d
Kairi Osawa <kairi.c9a8fa@gmail.com>
2023-07-15 15:32:06 +0900

    add hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+
+  # Applicationコントローラにhelloアクションを追加
+  def hello
+    render html: "Hello World"
+  end
+
+  def goodbye
+    render html: "Goodbye World"
+  end
 end
