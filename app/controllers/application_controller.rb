6a57557d8741b4cb4082039acbd7f8dcedb458fe
Kairi Osawa <kairi.c9a8fa@gmail.com>
2023-07-16 13:32:11 +0900

    Finish static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,2 @@
 class ApplicationController < ActionController::Base
-
-  # Applicationコントローラにhelloアクションを追加
-  def hello
-    render html: "Hello World"
-  end
-
-  def goodbye
-    render html: "Goodbye World"
-  end
 end
