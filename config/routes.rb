d59bd3ccd0e4ba75e4c6ba0becbd791a8d2e7108
Kairi Osawa <kairi.c9a8fa@gmail.com>
2023-12-30 21:58:38 +0900

    コンフリクト解消2

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,9 @@ Rails.application.routes.draw do
   # root "articles#index"
   # root "application#hello"
   # root "users#index"
+  root "static_pages#home"
+  get 'static_pages/home'
+  get 'static_pages/help'
+  get 'static_pages/about'
+  get 'static_pages/contact'
 end
